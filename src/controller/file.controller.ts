defe20307c11c18f74bcbf2c9da3d229f48c9253
rimpleshrestha <rimpleshrestha133@gmail.com>
2025-12-08 22:23:04 +0545

    file upload backend

---
+++ src/controller/file.controller.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+
+const uploadToCloud = async (req: Request, res: Response) => {};
+const deleteFromCloud = async (req: Request, res: Response) => {};
+export { uploadToCloud, deleteFromCloud };
