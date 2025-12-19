b40cf911f06fb3afb21377162a1c880d901f6754
rimpleshrestha <rimpleshrestha133@gmail.com>
2025-12-19 22:09:26 +0545

    user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,11 +4,15 @@ import {
   logout,
   refreshRecycle,
   signup,
+  upsertUserPin,
 } from "../controller/user.controller.js";
+import { verifyJWT } from "../../lib/jwt.js";
+import authorizeUser from "../middleware/verification.middleware.js";
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/login", login);
+router.post("/upsert-pin", authorizeUser, upsertUserPin);
 router.get("/logout", logout);
 router.get("/refresh-token", refreshRecycle);
 export default router;
