5ff2e423e1f6427812232a329959bc6d57dd5676
rimpleshrestha <rimpleshrestha133@gmail.com>
2026-01-02 21:12:53 +0545

    cloudinary for cloud upload

---
+++ lib/cloudinary.ts
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import path from "path";
+
+cloudinary.config({
+  cloud_name: "dtj0yjeni",
+  api_key: "316351566772248",
+  api_secret: process.env.VITE_CLOUDINARY_API_SECRET,
+});
+
+const uploadImageToCloudinary = async (localFilePath, publicId = null) => {
+  try {
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      public_id: publicId || path.parse(localFilePath).name,
+    });
+
+    fs.unlink(localFilePath, (err) => {
+      if (err) console.error("Failed to delete local image:", err);
+    });
+
+    return result;
+  } catch (error) {
+    console.error("Cloudinary upload failed:", error);
+    throw error;
+  }
+};
+
+module.exports = { uploadImageToCloudinary };
